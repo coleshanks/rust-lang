3ac336731070bb63b1b7df729636b13b580cf132
Cole Shanks <43489492+coleshanks@users.noreply.github.com>
2026-03-27 16:09:18 +0900

    working through ch3

---
+++ ch/Chapter3-Common-Programming-Concepts/functions/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function();
+    
+    let x = five();
+    println!("The value of x is: {x}");
+
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn five() -> i32 {
+    5
+}
